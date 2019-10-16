cfcc49190440bb84155b1a8e2aee0d965cf2f9f8
mugdha2195 <mugdha2195@gmail.com>
2019-10-16 11:22:08 +0530

    Parenthesis test

---
+++ src/com/mugdha2195/Exercise/Parenthesis.java
@@ -0,0 +1,31 @@
+package com.mugdha2195.Exercise;
+
+public class Parenthesis {
+
+	private boolean find(String array) {
+		int count = 0;
+		for (int i = 0; i < array.length(); i++) {
+			char ch = array.charAt(i);
+			if (ch == '(') {
+				count++;
+			} 
+			else if (ch == ')') {
+				count--;
+				if (count < 0) {
+					return false;
+				}
+			} else {
+				throw new IllegalArgumentException("Invalid character: " + ch);
+			}
+		}
+		return true;
+	}
+
+	public static void main (String args[]) {
+		Parenthesis p = new Parenthesis();
+		String s="((())())";
+		System.out.println(p.find(s));
+			
+	}
+	
+}
